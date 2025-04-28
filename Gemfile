c8b69cb42131368bff114ddbe96ecf65c79ba56d
Zachary J Ricker <109747756+Clovena@users.noreply.github.com>
2025-04-28 10:50:13 -0400

    Complete custom enumerables exercises.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rspec', '~> 3.10'
+
+gem 'rubocop', '~> 1.75'
+
+gem 'rubocop-performance', '~> 1.25'
--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,11 +1,74 @@
+# frozen_string_literal: true
+
+# Alter methods in Enumerable module
 module Enumerable
-  # Your code goes here
+  def my_each_with_index
+    self.length.times do |i| # rubocop:disable Style/RedundantSelf
+      yield self[i], i
+    end
+    self
+  end
+
+  def my_select
+    new = self.class.new
+    for item in self # rubocop:disable Style/For
+      new << item if yield item
+    end
+    new
+  end
+
+  def my_all?
+    for item in self # rubocop:disable Style/For
+      return false unless yield item
+    end
+    true
+  end
+
+  def my_any?
+    for item in self # rubocop:disable Style/For
+      return true if yield item
+    end
+    false
+  end
+
+  def my_none?
+    for item in self # rubocop:disable Style/For
+      return false if yield item
+    end
+    true
+  end
+
+  def my_count
+    result = 0
+    for item in self # rubocop:disable Style/For
+      return length unless block_given?
+
+      result += 1 if yield item
+    end
+    result
+  end
+
+  def my_map
+    new = self.class.new
+    for item in self # rubocop:disable Style/For
+      new << yield(item)
+    end
+    new
+  end
+
+  def my_inject(new)
+    for item in self # rubocop:disable Style/For
+      new = yield new, item
+    end
+    new
+  end
 end
 
-# You will first have to define my_each
-# on the Array class. Methods defined in
-# your enumerable module will have access
-# to this method
+# Used for my_each alone
 class Array
-  # Define my_each here
+  def my_each
+    for item in self # rubocop:disable Style/For
+      yield item
+    end
+  end
 end
